e7191551225193fde9cbc9514231f099d1c86ac8
Sabina Talibova <sabinatalibov1@gmail.com>
2017-10-30 22:51:52 -0700

    converted to Celsi

--- UserInterface.py
+++ UserInterface.py
@@ -14,7 +14,7 @@ class UserInterface():
     urltillcity="http://api.openweathermap.org/data/2.5/weather?q="
     print("Enter name of city")
     city=input()
-    apikey="&appid=00e8850255d526bfa5f02705e8c0ea07"
+    apikey="&units=metric&appid=00e8850255d526bfa5f02705e8c0ea07"
     wholeurl=urltillcity+city+apikey
 
 
@@ -28,7 +28,7 @@ class UserInterface():
     cityLabel=ttk.Label(win,text="Enter name of the city").grid(row=1,column=2)
     userCityInput=ttk.Entry(win).grid(row=2,column=2)
     submitButton=ttk.Button(win,text="Submit",width=10).grid(row=3,column=2)
-    temperatur=ttk.Label(win, text=tempish).grid(row=4,column=1)
+    temperatur=ttk.Label(win, text="Curernt Temperture in  "+city+ " is:  "+str(tempish)+" C").grid(row=4,column=1)
     win.mainloop()
 
 
