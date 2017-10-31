0022ffe4318858dc370fdcde99f6501816e68380
Sabina Talibova <sabinatalibov1@gmail.com>
2017-10-30 22:31:02 -0700

    gets input from user and prints temperature of given city(not oop yet)

---
+++ UserInterface.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import ttk
+import json
+import urllib.request
+
+class UserInterface():
+
+	
+    win=tk.Tk()
+    win.title(" Awesome Weather App")
+
+   
+
+    urltillcity="http://api.openweathermap.org/data/2.5/weather?q="
+    print("Enter name of city")
+    city=input()
+    apikey="&appid=00e8850255d526bfa5f02705e8c0ea07"
+    wholeurl=urltillcity+city+apikey
+
+
+    data=json.load(urllib.request.urlopen(wholeurl))
+
+    tempish=data['main']['temp']
+
+
+
+
+    cityLabel=ttk.Label(win,text="Enter name of the city").grid(row=1,column=2)
+    userCityInput=ttk.Entry(win).grid(row=2,column=2)
+    submitButton=ttk.Button(win,text="Submit",width=10).grid(row=3,column=2)
+    temperatur=ttk.Label(win, text=tempish).grid(row=4,column=1)
+    win.mainloop()
+
+
+
