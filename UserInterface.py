efd39f82990aae3f2c4eb1a80576dcd9553dc906
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2017-11-23 21:11:24 +0400

    Apply indentation

--- UserInterface.py
+++ UserInterface.py
@@ -4,32 +4,22 @@ import json
 import urllib.request
 
 class UserInterface():
-
 	
     win=tk.Tk()
     win.title(" Awesome Weather App")
 
-   
-
-    urltillcity="http://api.openweathermap.org/data/2.5/weather?q="
+    urltillcity = "http://api.openweathermap.org/data/2.5/weather?q="
     print("Enter name of city")
-    city=input()
-    apikey="&units=metric&appid=00e8850255d526bfa5f02705e8c0ea07"
-    wholeurl=urltillcity+city+apikey
+    city = input()
+    apikey = "&units=metric&appid=00e8850255d526bfa5f02705e8c0ea07"
+    wholeurl = urltillcity + city + apikey
 
+    data = json.load(urllib.request.urlopen(wholeurl))
 
-    data=json.load(urllib.request.urlopen(wholeurl))
+    temperature = data['main']['temp']
 
-    tempish=data['main']['temp']
-
-
-
-
-    cityLabel=ttk.Label(win,text="Enter name of the city").grid(row=1,column=2)
-    userCityInput=ttk.Entry(win).grid(row=2,column=2)
-    submitButton=ttk.Button(win,text="Submit",width=10).grid(row=3,column=2)
-    temperatur=ttk.Label(win, text="Curernt Temperture in  "+city+ " is:  "+str(tempish)+" C").grid(row=4,column=1)
+    cityLabel = ttk.Label(win, text="Enter name of the city").grid(row=1, column=2)
+    userCityInput = ttk.Entry(win).grid(row=2, column=2)
+    submitButton = ttk.Button(win,text="Submit",width=10).grid(row=3,column=2)
+    temperatur = ttk.Label(win, text="Current Temperature in " + city + " is: " + str(temperature) + " C").grid(row=4, column=1)
     win.mainloop()
-
-
-
